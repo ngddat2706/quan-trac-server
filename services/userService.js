72477f8f36be221fcbc2982e06f45b73d12fb5d5
MinhQuang <phungchiminhquang@gmail.com>
2022-03-28 15:52:07 +0700

    add validate function to register and login service

--- services/userService.js
+++ services/userService.js
@@ -1,41 +1,52 @@
 const User = require("../models/userModel.js");
 const bcrypt = require("bcryptjs");
 const auth = require("../middlewares/auth.js");
+const Joi = require('@hapi/joi');
 
-async function login(params, callback){
-    const {username, password} = params;
+
+const registerValidate =(body)=>{
+    const schema = Joi.object({
+        username:Joi.string().min(6).required(),
+        password:Joi.string().min(6).required(),
+    })
+    return schema.validate(body);
+        
+}
+
+const loginValidate =(body)=>{
+    const schema = Joi.object({
+        username:Joi.string().min(6).required(),
+        password:Joi.string().min(6).required(),
+    })
+    return schema.validate(body);
+}
+
+async function login(body, callback){
+    const {error} = loginValidate(body);
+    if(error)return callback(error.details[0].message);
+
+    const {username, password} = body;
     const user = await User.findOne({ username});
 
-    if(user != null && password != null){
-        if(bcrypt.compareSync(password, user.password)) {
-            const token = auth.generateAccessToken(username);
-            // call toJSON method applied during model instantiation
-            return callback(null, {...user.toJSON(), token});
-        }else{
-            return callback({
-                message: "Invalid Username/Password!",
-            });
-        }
+    if(bcrypt.compareSync(password, user.password)) {
+        const token = auth.generateAccessToken(username);
+        // call toJSON method applied during model instantiation
+        return callback(null, {...user.toJSON(), token});
     }else{
         return callback({
-            message:"Invalid Username/Password!",
+            message: "Invalid Username/Password!",
         });
     }
 }
 
-async function register(params, callback){
-    // if(params.username === undefined){
-    //     console.log(params.username);
-    //     return callback(
-    //         {
-    //             message: "Username Required",
-    //         },
-    //         ""
-    //     );
-    // }
-    //Quang will do a validate function later
+async function register(body, callback){
+
+    const {error} = registerValidate(body);
+    if(error)return callback(error.details[0].message);
     
-    const user = new User(params);
+    //check if name is already in the db
+    
+    const user = new User(body);
     user.save()
     .then((response)=>{
         return callback(null, response);
